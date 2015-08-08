a5ad266f0edcbcf35ccb7c4990d0e28f3638115b
arkingprog <arking@ukr.net>
2015-08-08 15:33:51 +0300

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, path: "auth", path_names:{sign_in: 'login',sign_out: 'logout'}
+    resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
+  root "todo_lists#index"
+end
